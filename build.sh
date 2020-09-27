11ee283fe2ac45039564cb6a2d54200d43dbcd0a
Jason Dsouza <jasonrdsouza@gmail.com>
2020-09-27 15:03:23 -0400

    Generated

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "Generating new site"
 pub run build_runner build --release --output web:docs
 cp CNAME docs/CNAME
 touch docs/.nojekyll
-rm docs/packages
+rm -rf docs/packages
 
 echo "Finished generating site into docs/"
 echo "Push to Github to deploy"
