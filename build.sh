ac00c331648fc48829ce06cd57f40e4e2cb37750
Jason Dsouza <jasonrdsouza@gmail.com>
2020-09-27 14:56:34 -0400

    Add build script and CNAME file for Github Pages

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+echo "Cleaning out old artifacts"
+rm -rf docs
+mkdir docs
+
+echo "Generating new site"
+pub run build_runner build --release --output web:docs
+cp CNAME docs/CNAME
+touch docs/.nojekyll
+rm docs/packages
+
+echo "Finished generating site into docs/"
+echo "Push to Github to deploy"
